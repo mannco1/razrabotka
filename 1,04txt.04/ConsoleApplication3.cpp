72767cd81451cda63dffb5d4089857bd1d00c94b
mannco1 <70008207+mannco1@users.noreply.github.com>
2023-05-25 15:50:35 +0300

    Add files via upload

---
+++ 1,04txt.04/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include"Header.h"
+#include <windows.h>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    setvbuf(stdout, nullptr, _IONBF, 0);
+
+    setlocale(LC_ALL, "Russian");
+    double A, B, C;
+
+
+
+
+
+    inter myobj;
+    myobj.Level1();
+    
+    
+
+
+  
+    return 0;
+}
+
+
